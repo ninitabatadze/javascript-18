c13bc1f3285b98591c720c5049639a076450068f
ninitabatadze <120283067+ninitabatadze@users.noreply.github.com>
2023-05-22 12:05:24 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function get_data() {
+    var el = document.getElementById("mybutton");
+
+    if (el.innerHTML == "ON") {
+        el.innerHTML = "OFF";
+    } else {
+        el.innerHTML = "ON";
+
+    }
+}
